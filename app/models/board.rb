61ee8221148e7c99a5e5eb1c42647a87b980de1e
Karim Butt <karim.butt@gmail.com>
2014-11-21 12:48:01 -0500

    linked pages and created models for picture to create color and picture objects

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,16 @@
 class Board < ActiveRecord::Base
 	has_many :pictures
 	has_many :colors, through: :pictures
+
+
+	def format(objects)
+		objects.collect do |object|
+			rgb = Color.find(object.color_id).rgba
+			prevalence = object.pixels_count.to_i
+			value = [rgb, prevalence]
+		
+		end
+	end
+
 end
 
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,36 +4,38 @@ class Picture < ActiveRecord::Base
 	has_many :colors, through: :picture_colors
   	mount_uploader :image, ImageUploader
 
+
   def parse_colors(image_path, colors=256, depth=8)
-    output = `convert #{image_path} -resize 400x400 -format %c -dither None -quantize YIQ -colors #{colors} -depth #{depth} histogram:info:-`
-    binding.pry
-    @lines = output.lines.sort.reverse.map(&:strip).reject(&:empty?)
-    colors
-    color_counts
-    scores
-    create_colors
+
+    img =  Magick::Image.read(image_path).first
+	pixels_and_counts = img.quantize.color_histogram
+
+	rgb_hashes = Hash.new
+
+	pixels_and_counts.each do |key, value|
+		fake_rgb = key.to_color(Magick::AllCompliance, false, Magick::QuantumDepth, true)
+		real_rgb = "##{fake_rgb[1..2]}#{fake_rgb[5..6]}#{fake_rgb[9..10]}"
+		rgb_hashes[real_rgb] = value
+	end
+
+	sorted_rgb = rgb_hashes.sort_by {|key, value| value}.reverse
+	
+	create_color_objects(sorted_rgb)
   end
 
-  # Returns an array of colors in descending order of occurances.
-  def colors
-    hex_values = @lines.map { |line| line[/#([0-9A-F]{6}) /, 1] }.compact
-    hex_values.map { |hex| Color::RGB.from_html(hex) }
+  def create_color_objects(sorted_rgb)
+  	sorted_rgb.each do |color_array|
+  		color_name = color_array[0]
+  		color = Color.find_or_create_by(rgba: color_name)
+  		
+  		PictureColor.new.tap do |c|
+  			c.color_id = color.id
+  			c.picture_id = self.id
+  			c.pixels_count = color_array[1]
+  			c.save
+  		end
+  	end
   end
 
-  def color_counts
-    @lines.map { |line| line.split(':')[0].to_i }
-  end
-
-  def scores
-    total = color_counts.inject(:+).to_f
-    scores = color_counts.map { |count| count / total }
-    @colors_array = scores.zip(colors)
-  end
-
-  def create_colors
-    @colors_array.each do |color_info|
-      Color.new(color_info)
-    end
-  end
-
+  
 end
